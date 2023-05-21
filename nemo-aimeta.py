25dff9a5a8b08500d91a911da7f9734f0d3d70ff
vtvrv <98350357+vtvrv@users.noreply.github.com>
2023-05-21 13:14:30 +0000

    Add files via upload

--- nemo-aimeta.py
+++ nemo-aimeta.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
 # coding: utf-8
 
-import locale, gettext, os
+import locale, gettext, os, sys, traceback, json
+from PIL import Image
+import piexif, piexif.helper
 
 try:
     from urllib import unquote
@@ -12,37 +14,99 @@ import gi
 gi.require_versions({'Gtk': '3.0', 'Nemo': '3.0'})
 from gi.repository import GObject, Gtk, Nemo
 
-from PIL import Image
-
 SUPPORTED_EXTENSIONS = [".jpg", ".jpeg", ".png", ".webp"] #ensure all lowercase
+
+
+#Patch
+#https://github.com/AUTOMATIC1111/stable-diffusion-webui/blob/master/modules/sd_samplers_kdiffusion.py
+class sd_samplers():
+    samplers_k_diffusion = [
+        ('Euler a', 'sample_euler_ancestral', ['k_euler_a', 'k_euler_ancestral'], {}),
+        ('Euler', 'sample_euler', ['k_euler'], {}),
+        ('LMS', 'sample_lms', ['k_lms'], {}),
+        ('Heun', 'sample_heun', ['k_heun'], {}),
+        ('DPM2', 'sample_dpm_2', ['k_dpm_2'], {'discard_next_to_last_sigma': True}),
+        ('DPM2 a', 'sample_dpm_2_ancestral', ['k_dpm_2_a'], {'discard_next_to_last_sigma': True}),
+        ('DPM++ 2S a', 'sample_dpmpp_2s_ancestral', ['k_dpmpp_2s_a'], {}),
+        ('DPM++ 2M', 'sample_dpmpp_2m', ['k_dpmpp_2m'], {}),
+        ('DPM++ SDE', 'sample_dpmpp_sde', ['k_dpmpp_sde'], {}),
+        ('DPM fast', 'sample_dpm_fast', ['k_dpm_fast'], {}),
+        ('DPM adaptive', 'sample_dpm_adaptive', ['k_dpm_ad'], {}),
+        ('LMS Karras', 'sample_lms', ['k_lms_ka'], {'scheduler': 'karras'}),
+        ('DPM2 Karras', 'sample_dpm_2', ['k_dpm_2_ka'], {'scheduler': 'karras', 'discard_next_to_last_sigma': True}),
+        ('DPM2 a Karras', 'sample_dpm_2_ancestral', ['k_dpm_2_a_ka'],
+         {'scheduler': 'karras', 'discard_next_to_last_sigma': True}),
+        ('DPM++ 2S a Karras', 'sample_dpmpp_2s_ancestral', ['k_dpmpp_2s_a_ka'], {'scheduler': 'karras'}),
+        ('DPM++ 2M Karras', 'sample_dpmpp_2m', ['k_dpmpp_2m_ka'], {'scheduler': 'karras'}),
+        ('DPM++ SDE Karras', 'sample_dpmpp_sde', ['k_dpmpp_sde_ka'], {'scheduler': 'karras'}),
+    ]
+
+    all_samplers = [
+        #*sd_samplers_compvis.samplers_data_compvis,
+        *samplers_k_diffusion
+    ]
+
+    samplers_map = {}
+    for sampler in all_samplers:
+        for i in sampler[2]:
+            samplers_map[i] = sampler[0]
+
+#https://github.com/AUTOMATIC1111/stable-diffusion-webui/blob/master/modules/images.py
+def read_info_from_image(image):
+    items = image.info or {}
+
+    geninfo = items.pop('parameters', None)
+
+    if "exif" in items:
+        exif = piexif.load(items["exif"])
+        exif_comment = (exif or {}).get("Exif", {}).get(piexif.ExifIFD.UserComment, b'')
+        try:
+            exif_comment = piexif.helper.UserComment.load(exif_comment)
+        except ValueError:
+            exif_comment = exif_comment.decode('utf8', errors="ignore")
+
+        if exif_comment:
+            items['exif comment'] = exif_comment
+            geninfo = exif_comment
+
+        for field in ['jfif', 'jfif_version', 'jfif_unit', 'jfif_density', 'dpi', 'exif',
+                      'loop', 'background', 'timestamp', 'duration']:
+            items.pop(field, None)
+
+    if items.get("Software", None) == "NovelAI":
+        try:
+            json_info = json.loads(items["Comment"])
+            sampler = sd_samplers.samplers_map.get(json_info["sampler"], "Euler a")
+
+            geninfo = f"""{items["Description"]}
+Negative prompt: {json_info["uc"]}
+Steps: {json_info["steps"]}, Sampler: {sampler}, CFG scale: {json_info["scale"]}, Seed: {json_info["seed"]}, Size: {image.width}x{image.height}, Clip skip: 2, ENSD: 31337"""
+        except Exception:
+            print("Error parsing NovelAI image generation parameters:", file=sys.stderr)
+            print(traceback.format_exc(), file=sys.stderr)
+
+    return geninfo, items
+
+def parse_geninfo(info):
+    info = info.replace('\\n', '\n') #Help with improperly embedded prompts
+    lines = info.rsplit('\n', 1)
+
+    setting_str = lines[-1]  # Assume last line is settings
+    result = {k: v for k, v in (x.split(": ") for x in setting_str.split(", "))}
+
+    if len(lines) > 1:  # contains positive or negative prompts
+        prompts = [x.rstrip('\n') for x in lines[0].split("Negative prompt: ", 1)]
+        result["Prompt"] = prompts[0]
+        if len(prompts) > 1:
+            result["Negative prompt"] = prompts[1]
+
+    return result
+
 
 def getinfo(filename):
     img = Image.open(filename)
-    extension = os.path.splitext(filename)[-1].lower()
-    if extension == '.png':
-        parameters = img.info['parameters']
-    elif extension in [".jpg", ".jpeg", ".webp"]:
-        parameters = img._getexif()[37510]  # 37510 is ExifID for "UserComment" https://exiftool.org/TagNames/EXIF.html
-        parameters = parameters.replace(b'\x00', b'').decode("utf-8")
-        if parameters.startswith("UNICODE"):
-            parameters = parameters[7:]
-    else:
-        return {}
-
-    parameters = parameters.replace('\\n', '\n')  # Help with improperly embedded prompts
-    lines = parameters.rsplit('\n', 1)
-
-    #Assume last line lines[-1] is string of settings
-    #lines[-1] looks like this "Step: 4, Model: sd1.5, CFG scale: 5"
-    info = {k: v for k, v in (x.split(": ") for x in lines[-1].split(", "))}
-
-    if len(lines) > 1:  # contains positive or negative prompts
-        prompts = [x.rstrip('\n') for x in lines[0].split("Negative prompt: ", 1)]  # [positive, negative]
-        info['Prompt'] = prompts[0]
-        if len(prompts) > 1:
-            info['Negative prompt'] = prompts[1]
-
-    return info
+    geninfo, _ = read_info_from_image(img)
+    return parse_geninfo(geninfo)
 
 GUI = """
 <interface>
